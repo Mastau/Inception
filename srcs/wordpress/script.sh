66627d201c6b09ebd5b3b65f7f3f0f95c282c92b
Thomas Arnaud <thomarna@student.42angouleme.fr>
2025-02-04 10:17:52 +0100

    Minors fix : Add exec "$@" in script & env var 4 init.sql

--- srcs/wordpress/script.sh
+++ srcs/wordpress/script.sh
@@ -6,5 +6,4 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=thomarna.42.fr --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=admin@admin.com --allow-root
-
- php-fpm7.4 -F
+exec "$@"
